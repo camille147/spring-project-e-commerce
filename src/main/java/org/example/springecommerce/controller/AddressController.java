ccae8cb62ecf9c224bae59937c5caa9ed58de16e
Camille <camille.pinault@coda-student.school>
2026-01-26 00:13:20 +0100

    chore: rename Adress to Address

--- src/main/java/org/example/springecommerce/controller/AddressController.java
+++ src/main/java/org/example/springecommerce/controller/AddressController.java
@@ -7,6 +7,7 @@ import org.example.shared.model.service.CustomUserDetails;
 import org.example.shared.repository.AddressRepository;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,24 +18,33 @@ public class AdressController {
     private AddressRepository addressRepository;
 
     @PostMapping("/user/address/add")
-    public String addAddress(@Valid @ModelAttribute("addressForm") AdressForm form,
-                             BindingResult result,
-                             @AuthenticationPrincipal CustomUserDetails currentUser) {
-
+    public String addAddress(
+            @Valid @ModelAttribute("addressForm") AdressForm form,
+            BindingResult result,
+            @AuthenticationPrincipal CustomUserDetails currentUser,
+            Model model
+    ) {
         if (result.hasErrors()) {
+            // On recharge les infos nécessaires au profil pour le réaffichage
+            model.addAttribute("user", currentUser.getUser());
+            model.addAttribute("userId", currentUser.getId());
             return "user/profile";
         }
 
-        // convers° Form -> Entity
+        // Convers° Form -> Entity
         Address entity = new Address();
         entity.setStreet(form.getStreet());
         entity.setCity(form.getCity());
         entity.setZipCode(form.getZipCode());
         entity.setCountry(form.getCountry());
-        entity.setUser(currentUser.getUser()); // liaison adress -> user connecté
+        entity.setIsActive(true);
 
+        // 3. Liaison avec l'utilisateur connecté
+        entity.setUser(currentUser.getUser());
 
+        // 4. Sauvegarde
         addressRepository.save(entity);
+
         return "redirect:/user/profile";
     }
 }
