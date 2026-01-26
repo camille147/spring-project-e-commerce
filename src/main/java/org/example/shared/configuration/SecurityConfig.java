1adcf2e50e4f2a051484b51a05fbdf80faccda3b
Camille <camille.pinault@coda-student.school>
2026-01-26 14:29:42 +0100

    refactor: update security configuration and application setup

--- src/main/java/org/example/shared/configuration/SecurityConfig.java
+++ src/main/java/org/example/shared/configuration/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
     public SecurityFilterChain webSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**", "/static/**","/css/**", "/files/**", "/js/**", "/login", "/images/**", "/fonts/**", "/register", "/user/navbar", "/error/**").permitAll()
+                        .requestMatchers("/h2-console/**", "/static/**","/css/**", "/files/**", "/js/**", "/login", "/images/**", "/fonts/**", "/register", "/user/navbar", "/error/**", "/logout").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/user/**").hasRole("USER")
                         .anyRequest().authenticated()
--- src/main/java/org/example/springecommerce/controller/ShopController.java
+++ src/main/java/org/example/springecommerce/controller/ShopController.java
@@ -3,11 +3,14 @@ package org.example.springecommerce.controller;
 import org.example.shared.model.entity.Product;
 import org.example.shared.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 
 @Controller
 public class ShopController {
@@ -16,11 +19,18 @@ public class ShopController {
     private ProductRepository productRepository;
 
     @GetMapping("/user/shop")
-    public String displayShop(Model model) {
-        List<Product> products = productRepository.findAll();
-        model.addAttribute("product", products);
+    public String displayShop(Model model,
+    @RequestParam(name = "page", defaultValue = "0") int page) {
+
+        Pageable pageable = PageRequest.of(page, 9);
+
+        Page<Product> productPage = productRepository.findAll(pageable);
+
+        model.addAttribute("product", productPage.getContent()); // La liste réduite des produits
+        model.addAttribute("currentPage", page);                  // La page actuelle (0, 1, 2...)
+        model.addAttribute("totalPages", productPage.getTotalPages()); // Le nombre total de pages
+
         return "user/shop";
     }
-
 
 }
